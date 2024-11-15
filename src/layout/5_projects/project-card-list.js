d76557cf0c8879ee31d24b4c10341866ba7b5e80
DanRBJ <d.r.bond.juez@hotmail.com>
2024-11-15 01:20:57 +0100

    Preliminary final version. Not responsive

--- src/layout/5_projects/project-card-list.js
+++ src/layout/5_projects/project-card-list.js
@@ -1,10 +1,10 @@
 import grupoValora from './project-cards/grupo-valora.json';
-import kaveHome from './project-cards/grupo-valora.json';
+import kaveHome from './project-cards/kave-home.json';
 import grupoFSM from './project-cards/grupo-fsm.json';
 import reservaDeLaBiosfera from './project-cards/reserva-de-la-biosfera.json';
 import lasVistas from './project-cards/las-vistas.json';
 import tangible from './project-cards/tangible.json';
-
+import dxtTenerife from './project-cards/dxt-Tenerife.json';
 
 
 
@@ -15,7 +15,7 @@ const projectInfoAndMediaList = [
     grupoFSM,
     reservaDeLaBiosfera,
     lasVistas,
-    tangible
-
+    tangible,
+    dxtTenerife
 ]
 export default projectInfoAndMediaList;
